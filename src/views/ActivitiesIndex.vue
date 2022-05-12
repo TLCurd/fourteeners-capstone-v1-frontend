c45bf3b191d7b3ab04996fcf5f2158b724040d31
TLCurd <TLCurdChiro@gmail.com>
2022-05-12 12:52:56 -0600

    add modals to activity index page w/ links to rec area show page

--- src/views/ActivitiesIndex.vue
+++ src/views/ActivitiesIndex.vue
@@ -5,7 +5,8 @@ export default {
   data: function () {
     return {
       message: "Activities Offered at Colorado's Outdoor Recreation Areas",
-      activities: []
+      activities: [],
+      currentActivity: {}
     };
   },
   created: function () {
@@ -19,14 +20,23 @@ export default {
         this.activities = response.data
       })
     },
+    showActivity: function (theActivity) {
+      console.log(theActivity);
+      console.log('showing Activity')
+      this.currentActivity = theActivity;
+      this.editActivityParams = theActivity;
+      document.querySelector("#activity-details").showModal();
+    },
   },
 };
 </script>
 
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
-
+    <h1><u>{{ message }}</u></h1>
+    <br>
+    <h4>Here, you can explore Colorado's federally managed recreation areas by choosing an activity of your liking. Just
+      click on an activity name and you can view all areas offering that activity!</h4>
     <div v-for="activity in activities" v-bind:key="activity.id">
       <!-- <img v-bind:src="recArea.photo"> -->
       <!-- <h2>
@@ -38,11 +48,29 @@ export default {
       <!-- <div v-for=" activity in recArea.activities">
         <p>Activities: {{ activity }}</p>
       </div> -->
-      <br>
-      <router-link v-bind:to="`/activities/${activity.id}`">{{
-          activity.name
-      }} </router-link>
-      <!-- <hr> -->
+
+      <li>
+        <button v-on:click="showActivity(activity)">{{ activity.name }}</button>
+        <br />
+      </li>
+
+      <dialog id="activity-details">
+        <form method="dialog">
+          <h1><u>Areas offering {{ currentActivity.name }}:</u></h1>
+          <div v-for="recArea in currentActivity.rec_areas">
+            <p>
+              <b>Recreational Area: </b><a v-bind:href="`/rec_areas/${recArea.id}`">{{ recArea.name }}</a>
+              <br>
+              <b>Contact information:</b> <i>{{ recArea.email }}</i> <b>|</b> <i>{{ recArea.phone_number }} </i>
+            </p>
+            <hr>
+          </div>
+
+
+          <button>Close</button>
+        </form>
+      </dialog>
+
     </div>
   </div>
 </template>
