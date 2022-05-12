e632ec861b5ef14113ed313de387d2ac89280485
TLCurd <TLCurdChiro@gmail.com>
2022-05-12 14:45:03 -0600

    makes columns/rows on activity index page

--- src/views/ActivitiesIndex.vue
+++ src/views/ActivitiesIndex.vue
@@ -37,49 +37,53 @@ export default {
     <br>
     <h4>Here, you can explore Colorado's federally managed recreation areas by choosing an activity of your liking. Just
       click on an activity name and you can view all areas offering that activity!</h4>
-    <div v-for="activity in activities" v-bind:key="activity.id">
-      <!-- <img v-bind:src="recArea.photo"> -->
-      <!-- <h2>
+    <div class="row">
+
+      <div class="col-sm-4 " v-for="activity in activities" v-bind:key="activity.id">
+        <!-- <img v-bind:src="recArea.photo"> -->
+        <!-- <h2>
         activity: {{ activity.name }}
 
 
       </h2> -->
 
-      <!-- <div v-for=" activity in recArea.activities">
+        <!-- <div v-for=" activity in recArea.activities">
         <p>Activities: {{ activity }}</p>
       </div> -->
 
-      <li>
-        <button v-on:click="showActivity(activity)">{{ activity.name }}</button>
-        <br />
-      </li>
+        <li>
+          <button v-on:click="showActivity(activity)">{{ activity.name }}</button>
+          <br />
+        </li>
 
-      <dialog id="activity-details">
-        <form method="dialog">
-          <h1><u>Areas offering {{ currentActivity.name }}:</u></h1>
-          <div v-for="recArea in currentActivity.rec_areas">
-            <p>
-              <b>Recreational Area: </b><a v-bind:href="`/rec_areas/${recArea.id}`">{{ recArea.name }}</a>
-              <br>
-              <b>Contact information: </b>
-              <i v-if="recArea.email">{{ recArea.email }}</i>
-              <i v-else>Not available</i>
-              <b> | </b>
-              <i v-if="recArea.phone_number">{{ recArea.phone_number }}</i>
-              <i v-else>Not available</i>
-              <!-- <b>Contact information:</b> <i>{{ recArea.email }}</i> <b>|</b> <i>{{
+        <dialog id="activity-details">
+          <form method="dialog">
+            <h1><u>Areas offering {{ currentActivity.name }}:</u></h1>
+            <div v-for="recArea in currentActivity.rec_areas">
+              <p>
+                <b>Recreational Area: </b><a v-bind:href="`/rec_areas/${recArea.id}`">{{ recArea.name }}</a>
+                <br>
+                <b>Contact information: </b>
+                <i v-if="recArea.email">{{ recArea.email }}</i>
+                <i v-else>Not available</i>
+                <b> | </b>
+                <i v-if="recArea.phone_number">{{ recArea.phone_number }}</i>
+                <i v-else>Not available</i>
+                <!-- <b>Contact information:</b> <i>{{ recArea.email }}</i> <b>|</b> <i>{{
                 recArea.phone_number
             }} </i> -->
-            </p>
-            <hr>
-          </div>
+              </p>
+              <hr>
+            </div>
 
 
-          <button>Close</button>
-        </form>
-      </dialog>
+            <button>Close</button>
+          </form>
+        </dialog>
 
+      </div>
     </div>
+
   </div>
 </template>
 
